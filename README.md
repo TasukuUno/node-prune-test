2e9df1d2fb7093d465b9fa3c27bfb40e562d7f17
TasukuUno <tsk.uno@gmail.com>
2020-02-12 15:26:25 +0900

    add sample codes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # node-prune-test
+
+This is a sample project for trying [node-prune](https://github.com/tj/node-prune)  
+My issue is node-prune@1.1.0 via gobinaries.com does not work fine on alpine image.
---
+++ index.js
@@ -0,0 +1,7 @@
+const ejs = require('ejs');
+const yaml = require('yaml');
+
+console.log(ejs);
+console.log(yaml);
+
+console.log('\n\nThis project is only for trying node-prune.\nThe source code is not meaningful.\n');
